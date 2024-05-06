bba10792dd0749e268a76c5f104c8dce5d6ab70b
Nick Wojcik <docubranch@gmail.com>
2024-05-06 15:47:30 -0400

    Update test menu on compsci

--- javascript/computer-science/overview.js
+++ javascript/computer-science/overview.js
@@ -2,6 +2,20 @@ function init() {
   document.querySelector(".login-box").addEventListener("click", (element) => {
     document.querySelector(".login-container").classList.add("hide");
   });
+
+  document
+    .querySelector("li.expandable")
+    .addEventListener("click", (element) => {
+      let menuItem = document.querySelector(".expandable ul");
+
+      if (menuItem.classList.contains("hidden")) {
+        console.log("remove");
+        menuItem.classList.remove("hidden");
+      } else {
+        console.log("add");
+        menuItem.classList.add("hidden");
+      }
+    });
 }
 
 init();
