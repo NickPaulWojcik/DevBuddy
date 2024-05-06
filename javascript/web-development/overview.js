718a5ffdf823eb0a0496a7f0b04bd4bdf3866dac
Nick Wojcik <docubranch@gmail.com>
2024-05-05 23:31:28 -0400

    Upload initial project

---
+++ javascript/web-development/overview.js
@@ -0,0 +1,7 @@
+function init() {
+  document.querySelector(".login-box").addEventListener("click", (element) => {
+    document.querySelector(".login-container").classList.add("hide");
+  });
+}
+
+init();
