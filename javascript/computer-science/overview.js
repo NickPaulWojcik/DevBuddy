99735fb62236920d3e09a41e52c62378627d4172
NickPaulWojcik <NickPaulWojcik@gmail.com>
2024-05-07 00:26:53 -0400

    Style login dialog on compsci overview

--- javascript/computer-science/overview.js
+++ javascript/computer-science/overview.js
@@ -7,13 +7,16 @@ function init() {
     .querySelector("li.expandable")
     .addEventListener("click", (element) => {
       let menuItem = document.querySelector(".expandable ul");
+      let menuParent = document.querySelector(".expandable");
 
       if (menuItem.classList.contains("hidden")) {
         console.log("remove");
         menuItem.classList.remove("hidden");
+        menuParent.classList.add("expanded");
       } else {
         console.log("add");
         menuItem.classList.add("hidden");
+        menuItem.classList.remove("expanded");
       }
     });
 }
